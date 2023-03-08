5dccb30e6cb827ecedbaa7c24f666ad0fbc0b0dd
Lucas Beluati <lucasbeluati@gmail.com>
2023-03-08 19:21:52 -0300

    Ultima modificación antes de enviar

--- CVjs.js
+++ CVjs.js
@@ -1,17 +1,5 @@
-display='none';
-
-if(display=='none'){
 
 document.getElementById('boton').addEventListener('click', function(){
 
     document.getElementById('info').style.display='block';
 });
-}
-
-if(display=='block'){
-
-document.getElementById('boton').addEventListener('click', function(){
-    
-    document.getElementById('info').style.display='none';
-});
-}
